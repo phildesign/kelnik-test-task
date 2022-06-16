3f25f8c59b98cd55c1aea60f950405c772e51299
Phil <phil.nice.23@gmail.com>
2022-06-16 20:08:38 +0300

    add render apartment

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,36 @@
-document.addEventListener('DOMContentLoaded', function () {});
+document.addEventListener('DOMContentLoaded', function () {
+	fetch('../api/db.json')
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			renderApartment(data.apartments);
+		});
+
+	const renderApartment = (apartments) => {
+		const apartmentsBox = document.querySelector('.apartments__box');
+		apartmentsBox.innerHTML = apartments.map((apartment) => {
+			return `
+		  <div class="apartments__item">
+		    <div class="apartments__row">
+		      <div class="apartments__col">
+		        <img src=${apartment.layout} alt="" class="apartments__item-img">
+		      </div>
+		      <div class="apartments__col">
+		        <div class="apartments__item-text">${apartment.flat}</div>
+		      </div>
+		      <div class="apartments__col">
+		        <div class="apartments__item-text">${apartment.square}</div>
+		      </div>
+		      <div class="apartments__col">
+		        <div class="apartments__item-text">${apartment.floor_current} из ${apartment.floor_count}</div>
+		      </div>
+		      <div class="apartments__col">
+		        <div class="apartments__item-text">${apartment.price}</div>
+		      </div>
+		    </div>
+		  </div>
+		`;
+		});
+	};
+});
